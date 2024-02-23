33ba311fb27abd499011fdfddf16e8673cd47ccf
Kieran <49917350+KRawl8@users.noreply.github.com>
2024-02-23 15:51:50 +0000

    Added some JS functionality

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-let boardArea = document.querySelector(".game-board");
+const Gameboard = (function () {
+  const boardArea = document.querySelector(".game-board");
 
-for (let i = 0; i < 9; i++) {
-  const square = document.createElement("div");
-  square.className = "square";
-  boardArea.appendChild(square);
-  console.log(i);
-}
+  let board = [];
+  const getBoard = () => board;
+
+  const populateBoard = () => {
+    for (let i = 0; i < 9; i++) {
+      board.push(i);
+    }
+  };
+  populateBoard();
+
+  const renderBoard = () => {
+    board.forEach((value) => {
+      const square = document.createElement("div");
+      square.className = "square";
+      square.dataset.index = value;
+      square.addEventListener("click", (target) => {
+        if (target.currentTarget.textContent == "") {
+          value = target.currentTarget.dataset.index;
+          // When I have the gamplay set up I need to update the boardItem, changePlayer and check for winner
+        }
+      });
+      boardArea.appendChild(square);
+    });
+  };
+  renderBoard();
+
+  const clearBoard = () => {
+    while (boardArea.firstChild) {
+      boardArea.removeChild(boardArea.firstChild);
+      console.log(boardArea.firstChild + "Removed");
+    }
+  };
+
+  const resetBoard = () => {};
+
+  return { getBoard, populateBoard, renderBoard, clearBoard };
+})();
