e4bc431fe480c87cd134cfee40a162fb53465ef5
Kieran <49917350+KRawl8@users.noreply.github.com>
2024-02-25 13:30:36 +0000

    Finished project

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ const Gameboard = (function () {
       square.addEventListener("click", (target) => {
         if (target.currentTarget.textContent == "") {
           value = target.currentTarget.dataset.index;
-          // When I have the gamplay set up I need to update the boardItem, changePlayer and check for winner
+          board[value] = Gameplay.getCurrentPlayer().token;
+
+          Gameplay.changePlayer(target);
+          Gameplay.checkForWins();
         }
       });
       boardArea.appendChild(square);
@@ -30,11 +33,80 @@ const Gameboard = (function () {
   const clearBoard = () => {
     while (boardArea.firstChild) {
       boardArea.removeChild(boardArea.firstChild);
-      console.log(boardArea.firstChild + "Removed");
+    }
+    board = [];
+    populateBoard();
+  };
+
+  const resetBoard = () => {
+    clearBoard();
+    renderBoard();
+  };
+
+  return { getBoard, populateBoard, resetBoard };
+})();
+
+const newPlayer = (name, token) => {
+  return { name, token };
+};
+
+const Gameplay = (function () {
+  const header = document.querySelector(".header");
+  const titleText = document.querySelector("h1");
+  const currentPlayerNumber = document.querySelector(".player-number");
+  const currentPlayerCounter = document.querySelector(".player-counter");
+  let resetButton = document.querySelector(".reset-button");
+
+  const playerOne = newPlayer("1", "X");
+  const playerTwo = newPlayer("2", "O");
+
+  let currentPlayer = playerOne;
+  const getCurrentPlayer = () => currentPlayer;
+
+  const changePlayer = (e) => {
+    if (currentPlayer === playerOne) {
+      e.currentTarget.textContent = currentPlayer.token;
+      currentPlayer = playerTwo;
+      currentPlayerNumber.textContent = currentPlayer.name;
+      currentPlayerCounter.textContent = currentPlayer.token;
+    } else if (currentPlayer === playerTwo) {
+      e.currentTarget.textContent = currentPlayer.token;
+      currentPlayer = playerOne;
+      currentPlayerNumber.textContent = currentPlayer.name;
+      currentPlayerCounter.textContent = currentPlayer.token;
     }
   };
 
-  const resetBoard = () => {};
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-  return { getBoard, populateBoard, renderBoard, clearBoard };
+  const checkForWins = () => {
+    winConditions.forEach((winCon) => {
+      if (
+        Gameboard.getBoard()[winCon[0]] === Gameboard.getBoard()[winCon[1]] &&
+        Gameboard.getBoard()[winCon[0]] === Gameboard.getBoard()[winCon[2]] &&
+        titleText.textContent === "Tic-Tac-Toe"
+      ) {
+        winner = getCurrentPlayer() === playerOne ? playerTwo : playerOne;
+        titleText.textContent = "Player " + winner.name + " Wins!";
+        header.style.backgroundColor = "rgb(221, 174, 20)";
+      }
+    });
+  };
+
+  resetButton.addEventListener("click", () => {
+    Gameboard.resetBoard();
+    titleText.textContent = "Tic-Tac-Toe";
+    header.style.backgroundColor = "rgb(17, 161, 36)";
+  });
+
+  return { getCurrentPlayer, changePlayer, checkForWins };
 })();
